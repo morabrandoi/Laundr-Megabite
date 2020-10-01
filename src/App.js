c4b4ba1449fe8c837810d78dce67d75cf6ad6db9
morabrandoi <morabrandoi@gmail.com>
2020-10-01 19:39:19 -0400

    added firebase instruction to repo

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
+          <br></br>
+          <br></br>
+          To deploy to firebase:
+          <br></br>make sure you have installed firebase tools via<br></br><br></br>
+          <code>npm install -g firebase-tools</code><br></br><br></br>and run 
+          <br></br><br></br><code>firebase login</code><br></br><br></br> Once that is set up, you only
+          ever need to run <br></br><br></br><code>firebase deploy</code><br></br><br></br>in the
+          terminal. Website should update shortly
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
