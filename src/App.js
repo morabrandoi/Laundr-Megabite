3ad9e1aaf62ea61f2c7868e6d3349b63efa2d394
Andrea Relova <andreamrelova@gmail.com>
2020-10-22 15:45:15 -0400

    finished navigation bar except for buttons that go to pages that don't exist yet

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Shops from './components/Shop/Shop';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="NaviBar">
-    <a href="/shop" className="NaviButtons">
-      <h2>Shop All</h2>
-    </a>
-    <h2>Our Story</h2>
-    <h2>Contact</h2>
-    <h2>FAQ</h2>
-    </div>
+        <div className="NaviBar">
+          <a href="/">
+          <img src="Laundr-Blue-Logo.png" alt="" className="LaundrBlue"/>
+          </a>
+          <a href="/shop" className="NaviButtons">
+            <h2>Shop All</h2>
+          </a>
+          <h2 className="NaviButtons">Our Story</h2>
+          <h2 className="NaviButtons">Contact</h2>
+          <h2 className="NaviButtons">FAQ</h2>
+          <div>
+            <AccountCircleIcon  className="Icons"/>
+            <ShoppingCartIcon className="Icons"/>
+          </div>
+        </div>
         <div>
           <Switch>
             <Route path="/" component={Home} exact/>
