b115cb19b9b050f49834e1c1dc82c7151d73e2c9
Andrea Relova <andreamrelova@gmail.com>
2020-10-22 13:50:02 -0400

    Started Navigation bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Shops from './components/Shop/Shop';
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+    <div className="NaviBar">
+    <h2>Shop All</h2>
+    <h2>Our Story</h2>
+    <h2>Contact</h2>
+    <h2>FAQ</h2>
+    </div>
         <div>
           <Switch>
             <Route path="/" component={Home} exact/>
