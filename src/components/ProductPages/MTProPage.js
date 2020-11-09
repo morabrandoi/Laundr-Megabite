707b87eb60425b7ea6850380ff5638c93179a1bd
wesleypetry <wesleypetry@ufl.edu>
2020-11-09 02:03:37 -0500

    Product Page Changes

--- src/components/ProductPages/MTProPage.js
+++ src/components/ProductPages/MTProPage.js
@@ -4,11 +4,15 @@ import styles from './styles.module.css'
 function MTProPage(props) {
     return (
         <>
-            <h1 className={styles.headerText}>Mahogany Teakwood Laundr Bomb</h1>
+            <div className={styles.masterDivMT}>
+                <h1 className={styles.headerText}>Mahogany Teakwood Laundr Bomb</h1>
+            </div>
 
+            <div className={styles.spacer}></div>
             <div className={styles.text}>
                 <a href="/"> Go Back to Home page</a>
             </div>
+            <div className={styles.spacer}></div>
             
         </>
     );
