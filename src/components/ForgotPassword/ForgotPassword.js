ee6917b9d471fa3d1d93c003382e9ea5ef6cd100
Andrea Relova <andreamrelova@gmail.com>
2020-12-08 22:19:41 -0500

    Started Forgot Password => MaterialUI version

--- src/components/ForgotPassword/ForgotPassword.js
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Card, Container, Alert } from "react-bootstrap"
+import { TextField, Button, Card, Container } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert';
 import { useAuth } from '../../contexts/AuthContext'
 import { Link } from 'react-router-dom'
 
@@ -7,11 +8,12 @@ import { Link } from 'react-router-dom'
 export default function ForgotPassword() {
 
   const emailRef = useRef()
-  
+
   const { resetPassword } = useAuth()
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState("")
+  const [email, setEmail] = useState("");
 
   async function handleSubmit(event) {
     event.preventDefault()
@@ -20,13 +22,13 @@ export default function ForgotPassword() {
         setMessage("")
         setError('')
         setLoading(true)
-        await resetPassword(emailRef.current.value)
+        await resetPassword(email)
         setMessage('Check your inbox for further instructions.')
     } catch {
       setError('Failed to send reset email')
     }
     setLoading(false)
-    
+
   }
 
   return (
@@ -35,22 +37,16 @@ export default function ForgotPassword() {
       style={{minHeight: "60vh"}}>
       <div className="w-100" style={{maxWidth: "400px"}}>
         <Card>
-          <Card.Body>
             <h2 className="text-center mb-4">Password Reset</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            {message && <Alert variant="success">{message}</Alert>}
-            <Form onSubmit={ handleSubmit }>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} required></Form.Control>
-              </Form.Group>
+            {error && <Alert severity="error">{error}</Alert>}
+            {message && <Alert severity="success">{message}</Alert>}
+            <form onSubmit={ handleSubmit }>
+              <TextField variant="standard" label="Email" required={true} value={email} onChange={(e)=>{e.preventDefault(); setEmail(e.target.value)}}/>
               <Button disabled={loading} className="w-100" type="submit">Reset Password</Button>
-            </Form>
+            </form>
             <div className="w-100 text-center mt-3">
               <Link to='/signin'>Sign In</Link>
-
             </div>
-          </Card.Body>
         </Card>
         <div className="w-100 text-center mt-2">
           Need an account? <Link to="/signup">Sign Up</Link>
@@ -60,4 +56,3 @@ export default function ForgotPassword() {
     </>
   )
 }
-
